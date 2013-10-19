897eb6b007c0d889a317b6ea2384bb6dae8f5340
Johan Nilsson <jonh04@gmail.com>
2013-10-19 18:10:52 -0400

    added nuspec and built first nuget version

--- src/HtmlToMarkdown.Net/Properties/AssemblyInfo.cs
+++ src/HtmlToMarkdown.Net/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HtmlToMarkdown.Net")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Converts Html to Markdown")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Johan Nilsson")]
 [assembly: AssemblyProduct("HtmlToMarkdown.Net")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
